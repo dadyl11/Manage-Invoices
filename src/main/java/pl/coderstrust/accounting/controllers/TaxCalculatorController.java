12859bd5e3bcaeedcb67016aac1ba0759b44bbe9
PhineasGage <mateuszgorski@gmail.com>
2018-07-29 10:56:11 +0200

    TaxController, taxservice

---
+++ src/main/java/pl/coderstrust/accounting/controllers/TaxCalculatorController.java
@@ -0,0 +1,10 @@
+package pl.coderstrust.accounting.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/calculator")
+@RestController
+public class TaxCalculatorController {
+
+}
---
+++ src/main/java/pl/coderstrust/accounting/logic/TaxCalculatorService.java
@@ -0,0 +1,8 @@
+package pl.coderstrust.accounting.logic;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaxCalculatorService {
+
+}
